b6d56aa13e7fee3f2b65aa9599109b3f1ae944ff
Rafael Perroni <rafaelperroni89@gmail.com>
2026-02-01 01:21:52 -0300

    fix

--- api/login.js
+++ api/login.js
@@ -1,5 +1,5 @@
 // api/login.js
-import querystring from 'querystring';
+import querystring from "querystring";
 
 /**
  * Este endpoint redireciona o usuário para a página de autorização do Spotify.
@@ -11,15 +11,15 @@ export default function handler(req, res) {
 
   const host = req.headers.host;
   // On Vercel, `x-forwarded-proto` is 'https'. Locally, it's undefined, so we default to 'http'.
-  const protocol = req.headers['x-forwarded-proto'] ? 'https' : 'http';
-  const REDIRECT_URI = `${protocol}://${host}/api/callback`;
+  const protocol = req.headers["x-forwarded-proto"] ? "https" : "http";
+  const REDIRECT_URI = `https://spotilike.vercel.app/api/callback`;
 
   // O 'scope' define as permissões que estamos solicitando ao usuário.
   const scope =
-    'streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state';
+    "streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state";
 
   const params = querystring.stringify({
-    response_type: 'code',
+    response_type: "code",
     client_id: SPOTIFY_CLIENT_ID,
     scope: scope,
     redirect_uri: REDIRECT_URI,
